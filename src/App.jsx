86d3475a74500707a4593ee3c3449284aa8e6bef
Jubair Ahmed <jubairahmed3000@gmail.com>
2023-05-11 12:31:38 +0530

    Adding Authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn,
+  SignIn,
+  SignUp,
+  UserButton,
+} from "@clerk/clerk-react";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+ 
+const clerkPubKey = "pk_test_YWxpdmUtd2Vhc2VsLTk0LmNsZXJrLmFjY291bnRzLmRldiQ";
 
+function PublicPage() {
   return (
-    <>
-      <h2>Hello This is Vite app</h2>
-    </>
-  )
+    <div>
+      <h1>Public page</h1>
+      <a href="/protected">Go to protected page</a>
+    </div>
+  );
 }
 
-export default App
+function ProtectedPage() {
+  return (
+    <div>
+      <h1>Protected page</h1>
+      <UserButton />
+    </div>
+  );
+}
+
+function ClerkProviderWithRoutes() {
+  const navigate = useNavigate();
+
+  return (
+    <ClerkProvider
+      publishableKey={clerkPubKey}
+      navigate={(to) => navigate(to)}
+    >
+      <Routes>
+        <Route path="/" element={<PublicPage />} />
+        <Route
+          path="/sign-in/*"
+          element={<SignIn routing="path" path="/sign-in" />}
+        />
+        <Route
+          path="/sign-up/*"
+          element={<SignUp routing="path" path="/sign-up" />}
+        />
+        <Route
+          path="/protected"
+          element={
+          <>
+            <SignedIn>
+              <ProtectedPage />
+            </SignedIn>
+             <SignedOut>
+              <RedirectToSignIn />
+           </SignedOut>
+          </>
+          }
+        />
+      </Routes>
+    </ClerkProvider>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <ClerkProviderWithRoutes />
+    </BrowserRouter>
+  );
+}
+
+export default App;
